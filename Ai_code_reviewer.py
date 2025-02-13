1519a1c1596dbaf3cceacadb4097e415b497c04e
K.Saidivya <saidivyakodipaka856@gmail.com>
2025-02-13 12:21:46 +0530

    Update Ai_code_reviewer.py

--- Ai_code_reviewer.py
+++ Ai_code_reviewer.py
@@ -7,7 +7,9 @@ system_prompt = """You are an AI-powered Python Code Reviewer.
 Your primary role is to analyze submitted Python code, identify potential bugs, suggest improvements,
  and provide optimized and corrected code snippets."""
 
-ai.configure(api_key= "API KEY")
+api_key = st.secrets["AIzaSyAVMS7VfIcpm_B6d-SYWiU_GVwjbdj3HB8"]
+ai.configure(api_key=api_key)
+
 model = ai.GenerativeModel(model_name="models/gemini-1.5-flash",system_instruction = system_prompt)
 
 st.title(":speech_balloon: An AI Code Reviewer")
